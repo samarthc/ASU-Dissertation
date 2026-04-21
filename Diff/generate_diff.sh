96abd7f55eac165e909c032016aec16eae0d9b66
Samarth Chawla <samarth10@gmail.com>
2026-04-20 19:26:23 -0700

    Preserves diff generation commands in zsh script.

--- Diff/generate_diff.sh
+++ Diff/generate_diff.sh
@@ -1,12 +1,15 @@
 #!/bin/zsh
 
+## TODO: make diffing bibliography data/style changes possible.
+
 ROOT=$(git rev-parse --show-toplevel)
 workdir="$(realpath $ROOT/Diff)"
-cd "$workdir"
+cd "$ROOT"
 
 ## Command used to generate diff between current version and pre-review version
 # This did not work as expected: when the paragraph with changes spanned two pages, it often output the incorrect one. For instance, let's say page N ends in the middle of a paragraph, which continues at the beginning of page N+1. The change is near the end of the paragraph (i.e. on page N+1), but the ONLYCHANGEDPAGE subtype excludes page N+1 and outputs page N.
 #git latexdiff --main dissertation.tex --verbose --quiet --latexmk --no-cleanup --math-markup=3 --type=CULINECHBAR --driver=xetex --subtype=ONLYCHANGEDPAGE --graphics-markup=0 pre-review
+##
 
 # The ZLABEL subtype tags each changed page in the aux file. The preamble extension from latexdiff contains instructions on how to use the aux file to extract only the changed pages from the compiled PDF.
 
@@ -16,13 +19,29 @@ git latexdiff --whole-tree --main dissertation.tex --verbose --quiet --latexmk -
 
 echo "Done."
 echo "Moving tmp files from "$workdir"/git-latexdiff.$(cat "$workdir"/tmpsuffix) to "$workdir""
-mv "$workdir/git-latexdiff.$(cat "$workdir"/tmpsuffix)/*" "$workdir"
+mv "$workdir"/git-latexdiff.$(cat "$workdir"/tmpsuffix)/* "$workdir" || {echo "Something went wrong moving tmp files -- exiting early" >&2; exit 1}
 
-"Removing git-latexdiff.$(cat "$workdir"/tmpsuffix)..."
+echo "Removing git-latexdiff.$(cat "$workdir"/tmpsuffix)..."
 rm -r "$workdir"/git-latexdiff.$(cat $workdir/tmpsuffix)
 
 echo "Done."
 
 ## Script for extracting only changed pages
 
-pdftk dissertation.pdf cat $(perl -lne 'if (m/\\zref\@newlabel{DIFchgb(\d*)}{.*\\abspage{(\d*)}}/ ) { $start{$1}=$2; print $2 } if (m/\\zref\@newlabel{DIFchge(\d*)}{.*\\abspage{(\d*)}}/) {if (defined($start{$1})) {for ($j=$start{$1}; $j<=$2; $j++) {print "$j";} } else {print "$2" } }' "$workdir/new/dissertation.aux" | uniq | tr \\n ' ') output dissertation-onlychanges.pdf
+echo "Extracting only changed pages into $workdir/dissertation-onlychanges.pdf"
+echo "..."
+
+pdftk "$workdir"/dissertation.pdf cat $(perl -lne 'if (m/\\zref\@newlabel{DIFchgb(\d*)}{.*\\abspage{(\d*)}}/ ) { $start{$1}=$2; print $2 } if (m/\\zref\@newlabel{DIFchge(\d*)}{.*\\abspage{(\d*)}}/) {if (defined($start{$1})) {for ($j=$start{$1}; $j<=$2; $j++) {print "$j";} } else {print "$2" } }' "$workdir/new/dissertation.aux" | uniq | tr \\n ' ') output "$workdir"/dissertation-onlychanges.pdf
+
+echo "Done."
+read -p "Remove generated diff with redundant pages? [y/n]: " -n 1 yn
+case $yn in 
+    [Yy])
+        echo "Removing redundant diff...";
+        rm "$workdir"/dissertation.pdf || {echo "Something wenet wrong trying to delete redundant diff" >&2; exit 1}
+        ;;
+    [Nn])
+        echo "Keeping redundant diff.";;
+    *)
+        echo "Invalid selection. Keeping redundant diff.";;
+esac
